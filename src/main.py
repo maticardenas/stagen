c53c95ee5d267208cdf643b8b60603935082fbac
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-11-23 17:42:27 +0100

    feat: adding initial scripts and TestNode class

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("test_text", TextType.NORMAL)
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
