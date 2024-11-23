64b7dca83aabf139c0180df3af9429a4b071bb9a
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-11-23 17:57:07 +0100

    feat: adding LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,3 +15,20 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag: str | None, value: str, props: dict = None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError("LeafNode value cannot be empty")  
+        if self.tag is None:
+            return self.value
+        
+        space = " " if self.props else ""
+
+        return f"<{self.tag}{space}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -13,3 +13,22 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr_with_children(self):
         node = HTMLNode("div", children=[HTMLNode("p", "Hello, World!")])
         self.assertEqual(str(node), "HTMLNode(div, None, [HTMLNode(p, Hello, World!, [], {})], {})")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "Hello, World!")
+        self.assertEqual(node.to_html(), "<p>Hello, World!</p>")
+    
+    def test_to_html_with_props(self):
+        node = LeafNode("p", "Hello, World!", {"class": "paragraph"})
+        self.assertEqual(node.to_html(), '<p class="paragraph">Hello, World!</p>')
+    
+    def test_to_html_with_empty_value(self):
+        node = LeafNode("p", "")
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_to_html_with_no_tag(self):
+        node = LeafNode(None, "Hello, World!")
+        self.assertEqual(node.to_html(), "Hello, World!")
